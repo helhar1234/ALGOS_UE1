62a1b3129f2b783b77e8032ae9b3362bd9fea982
elektroleena <if22b060@technikum-wien.at>
2023-05-18 15:35:19 +0200

    klasse routes

--- main.py
+++ main.py
@@ -39,11 +39,12 @@ def create_network_from_file(filename):
 
 programm_description()
 
+routes = ShortestRoutes()
 
 while True:
     command = input(bcolors.HEADER + bcolors.RED + "path_finder: " + bcolors.RESET)
     if not command.strip():
-        print(bcolors.WARNING + "\nUngültiger Command!" + bcolors.RESET)
+        print(bcolors.WARNING + "\nUngültiger Befehl!" + bcolors.RESET)
         continue
 
     split_input = shlex.split(command)
@@ -53,7 +54,7 @@ while True:
         break
 
     if len(split_input) < 3:
-        print(bcolors.WARNING + "\nUngültiger Command!" + bcolors.RESET)
+        print(bcolors.WARNING + "\nUngültiger Befehl!" + bcolors.RESET)
         continue
 
     if not split_input[0].endswith(".txt"):
@@ -62,12 +63,19 @@ while True:
     if check_file_exists(split_input[0]):
         network = create_network_from_file(split_input[0])
         if network.station_exists(split_input[1]) and network.station_exists(split_input[2]):
-            # network.show_network()
-
-            route, total_time = find_shortest_route_with_dijkstra(network, split_input[1], split_input[2])
+            existing_route = routes.get_route(split_input[1], split_input[2])
+            if existing_route:
+                route, total_time = existing_route
+                print("Existing route!!")
+            else:
+                router = DijkstraRouter(network)
+                route, total_time = router.find_shortest_route(split_input[1], split_input[2])
+                network.add_route(split_input[1], split_input[2], (route, total_time))
             print_route(route, total_time)
         else:
             if not network.station_exists(split_input[1]):
                 print(bcolors.WARNING + f"\nUngültige Station: {split_input[1]}!" + bcolors.RESET)
             else:
                 print(bcolors.WARNING + f"\nUngültige Station: {split_input[2]}!" + bcolors.RESET)
+    else:
+        print(bcolors.WARNING + f"\nDatei nicht gefunden: {split_input[0]}" + bcolors.RESET)
--- shortestRoute.py
+++ shortestRoute.py
@@ -1,76 +1,77 @@
-# Die Funktion implementiert den Dijkstra-Algorithmus, um den kürzesten Weg zwischen zwei Stationen
-# in einem gegebenen Netzwerk von Stationen und Verbindungen zu finden.
-def find_shortest_route_with_dijkstra(network, start, end):
-    shortest_distances = {station: float('inf') for station in
-                          network.network}  # Initialisiere die kürzesten Distanzen zu allen Stationen als unendlich, damit es bei jeder Distanz sofort anschlägt
-    previous_stations = {station: None for station in
-                         network.network}  # Initialisiere die vorherigen Stationen auf dem kürzesten Pfad als None
-    shortest_distances[start] = 0  # Setze die kürzeste Distanz zur Startstation auf 0
-    unvisited_stations = network.network.copy()  # Erstelle eine Kopie des Netzwerks, um die unbesuchten Stationen zu verfolgen (alle sind am anfang unbesucht)
+class ShortestRoutes:
+    def __init__(self):
+        self.routes = {}
 
-    while unvisited_stations:
-        current_station = min(unvisited_stations, key=lambda station: shortest_distances[
-            station])  # Wähle die Station mit der kürzesten Distanz als aktuelle Station
-        if shortest_distances[current_station] == float(
-                'inf'):  # Wenn die kürzeste Distanz zur aktuellen Station unendlich ist (kein Pfad vorhanden)
-            break  # Beende die Schleife
-        for neighbour, (distance, _) in network.network[
-            current_station].items():  # Für jede Nachbarstation der aktuellen Station
-            if neighbour in unvisited_stations:  # Wenn die Nachbarstation noch nicht besucht wurde
-                new_distance = shortest_distances[
-                                   current_station] + distance  # Berechne die neue Distanz zur Nachbarstation
-                if new_distance < shortest_distances[
-                    neighbour]:  # Wenn die neue Distanz kürzer ist als die bisherige kürzeste Distanz zur Nachbarstation
-                    shortest_distances[neighbour] = new_distance  # Aktualisiere die kürzeste Distanz zur Nachbarstation
-                    previous_stations[neighbour] = (current_station, network.network[current_station][neighbour][1],
-                                                    distance)  # Aktualisiere die vorherige Station auf dem kürzesten Pfad zur Nachbarstation
-        unvisited_stations.pop(current_station)  # Entferne die aktuelle Station aus den unbesuchten Stationen
+    def add_station(self, station):
+        self.routes[station] = {}
 
-    path = []  # Initialisiere eine leere Liste für den Pfad
-    current_station = end  # Setze die aktuelle Station auf die Endstation
-    while current_station is not None:  # Solange die aktuelle Station nicht None ist (d.h. solange wir nicht am Startpunkt sind)
+    def add_connection(self, station1, station2, distance, line):
+        self.routes[station1][station2] = (distance, line)
 
-        # Füge die aktuelle Station, die Linie und die Zeit zum Pfad hinzu
-        path.append((current_station,
-                     previous_stations[current_station][1] if previous_stations[current_station] else None,
-                     previous_stations[current_station][2] if previous_stations[current_station] else 0))
-        current_station = previous_stations[current_station][0] if previous_stations[
-            current_station] else None  # Setze die aktuelle Station auf die vorherige Station auf dem kürzesten Pfad
-    path = path[::-1]  # Kehre die Reihenfolge des Pfads um, um den richtigen Weg vom Start zum Ziel zu erhalten
+    def get_route(self, start, end):
+        if start in self.routes and end in self.routes[start]:
+            return self.routes[start][end]
+        else:
+            return None
 
-    return path, shortest_distances[end]  # Gib den Pfad und die kürzeste Distanz zum Ziel zurück
+    def add_route(self, start, end, route):
+        if start not in self.routes:
+            self.routes[start] = {}
+        self.routes[start][end] = route
 
 
-from colors import *
+class DijkstraRouter:
+    def __init__(self, network):
+        self.network = network
+
+    def find_shortest_route(self, start, end):
+        shortest_distances = {station: float('inf') for station in self.network.network}
+        previous_stations = {station: None for station in self.network.network}
+        shortest_distances[start] = 0
+        unvisited_stations = self.network.network.copy()
+
+        while unvisited_stations:
+            current_station = min(unvisited_stations, key=lambda station: shortest_distances[station])
+            if shortest_distances[current_station] == float('inf'):
+                break
+            for neighbour, (distance, _) in self.network.network[current_station].items():
+                if neighbour in unvisited_stations:
+                    new_distance = shortest_distances[current_station] + distance
+                    if new_distance < shortest_distances[neighbour]:
+                        shortest_distances[neighbour] = new_distance
+                        previous_stations[neighbour] = (current_station, self.network.network[current_station][neighbour][1], distance)
+            unvisited_stations.pop(current_station)
+
+        path = []
+        current_station = end
+        while current_station is not None:
+            path.append((current_station, previous_stations[current_station][1] if previous_stations[current_station] else None, previous_stations[current_station][2] if previous_stations[current_station] else 0))
+            current_station = previous_stations[current_station][0] if previous_stations[current_station] else None
+        path = path[::-1]
+
+        return path, shortest_distances[end]
 
 
-# Funktion, die die kürzeste Route übersichtlich ausgibt
 def print_route(route, total_time):
-    #  Überschriften und Grundinformationen zur Route
-    print(f"\n\n{bcolors.BOLD + bcolors.CYAN}{'-' * 40}\nROUTEN INFORMATION:\n{'-' * 40}{bcolors.RESET}\n")
-    print(
-        f"{bcolors.BOLD + bcolors.WHITE}Die kürzeste Route von {bcolors.UNDERLINE + bcolors.RED + route[0][0] + bcolors.RESET + bcolors.BOLD + bcolors.WHITE}🚩 zu {bcolors.UNDERLINE + bcolors.BLUE + route[-1][0] + bcolors.RESET + bcolors.BOLD + bcolors.WHITE}🏁 ist:{bcolors.RESET}\n")
-    print(f"    {bcolors.WHITE}Startstation ist {bcolors.UNDERLINE + bcolors.RED + route[0][0]}{bcolors.RESET}🚩\n")
+    print(f"Die kürzeste Route von {route[0][0]} zu {route[-1][0]} ist:")
+    print(f"Startstation ist {route[0][0]}")
 
-    current_line = route[0][1]  # Aktuelle Line (U-Bahn oder Straßenbahn)
-    line_route = f"    {bcolors.PURPLE + bcolors.UNDERLINE}{current_line}: {bcolors.RESET}" if current_line is not None else ""  # Aktuelle Line wird ausgegeben, (außer sie ist NULL bei der Startstation, wo man NOCH in keiner Line ist)
+    current_line = route[0][1]
+    line_route = f"{current_line}: " if current_line is not None else ""
 
     for i in range(len(route)):
-        station, line, time = route[i]  # Stops werden aufgeteilt und in die Variablen Station, Line und Time unterteilt
-        if i == 0:  # Startstation soll nicht separat ausgegeben werden
+        station, line, time = route[i]
+        if i == 0:
             continue
-        if line != current_line:    # wenn umgestiegen werden soll
-            if current_line is not None:    #   wird nur ausgegeben, wenn der Umstieg nicht die bei der Startstation ist
-                print(line_route.rstrip())  # unnötige Leerzeichen werden von der Route der einzelnen Line entfernt
-                print(f"    {bcolors.UNDERLINE + bcolors.BOLD + bcolors.ORANGE}Umstieg{bcolors.RESET + bcolors.WHITE}: Steige von Linie {bcolors.UNDERLINE + bcolors.PURPLE + current_line + bcolors.RESET + bcolors.WHITE} um auf Linie {bcolors.UNDERLINE + bcolors.PURPLE + line + bcolors.RESET + bcolors.WHITE} in der Station {bcolors.UNDERLINE + bcolors.ORANGE + route[i - 1][0]}{bcolors.RESET}\n")
-            current_line = line # Neue Line wird auf current Line gesetzt
-            # Line Route wird aufgebaut aus den Informationen
-            line_route = f"    {bcolors.UNDERLINE + bcolors.PURPLE + current_line + bcolors.RESET}: {bcolors.WHITE}{route[i - 1][0]} ({time} min') {station}{bcolors.RESET} "
-        else:   # wenn nicht umgestiegen werden muss
-            line_route += f"{bcolors.WHITE} ({time} min') {station}{bcolors.RESET}" # wird die neue station + Zeit einfach dran gehängt
+        if line != current_line:
+            if current_line is not None:
+                print(line_route.rstrip())
+                print(f"Steige von Linie {current_line} um auf Linie {line} in der Station {route[i - 1][0]}")
+            current_line = line
+            line_route = f"{current_line}: {route[i - 1][0]} ({time} min') {station} "
+        else:
+            line_route += f" ({time} min') {station}"
 
     print(line_route.rstrip())
-    # Endausgabe
-    print(f"\n    {bcolors.WHITE}Zielstation {bcolors.UNDERLINE + bcolors.BLUE + route[-1][0] + bcolors.RESET + bcolors.WHITE}🏁 wurde erreicht!{bcolors.RESET}")
-    print(f"\n{bcolors.BOLD + bcolors.WHITE}Gesamte Fahrzeit: {bcolors.GREEN + str(total_time)} Minuten{bcolors.RESET}\n")
-    print(f"{bcolors.BOLD + bcolors.CYAN}{'-' * 40}{bcolors.RESET}\n\n")
+    print(f"Zielstation {route[-1][0]} wurde erreicht!")
+    print(f"Gesamte Fahrzeit: {total_time} Minuten")
